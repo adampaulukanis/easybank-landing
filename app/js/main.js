4f0eeb15f2dd9efcf33fb27a72708af2ae977b02
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-02-18 13:14:20 +0100

    Animated Hamburger Menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.onclick = () => {
+    if (header.classList.contains("open")) {
+        // Close Hamburger Menu
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    } else {
+        // Open Hamburger Menu
+        header.classList.add("open");
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+};
