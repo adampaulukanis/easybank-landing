5987f22b0b5b2769d2fd2f1e8b411b794d536e3e
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-02-18 23:51:16 +0100

    Animated Mobile Menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,25 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.onclick = () => {
     if (header.classList.contains("open")) {
         // Close Hamburger Menu
+        body.classList.remove("noscroll");
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+        fadeElems.forEach((element) => {
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
     } else {
         // Open Hamburger Menu
+        body.classList.add("noscroll");
         header.classList.add("open");
-        overlay.classList.remove("fade-out");
-        overlay.classList.add("fade-in");
+        fadeElems.forEach((element) => {
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+        });
     }
 };
